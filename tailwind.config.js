bcd85d171425a96d77f2ff054a8b177fef246a44
sydneyIdundun <sydney.idundun@gmail.com>
2022-11-08 00:13:28 +0400

    added a gitignore file

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+"index.html",
+"shotgun/text.html",
+"shotgun/mytest.jsx"
+],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
