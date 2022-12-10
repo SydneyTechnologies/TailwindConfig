fed2d983dd4421584505cc7415d27d253379dbfb
Harith Onigemo <onigemotosin@gmail.com>
2022-12-10 15:04:41 +0400

    Switching branches

--- utils.py
+++ utils.py
@@ -1,5 +1,43 @@
 # this python script contains utility functions used in the main CLI script [tailwindConfig.py]
-import os 
+import os
+import re
+
+# import logging
+from watchdog.observers import Observer
+from watchdog.events import RegexMatchingEventHandler, PatternMatchingEventHandler
+
+
+# logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+changeList = []
+path = "."
+patterns = [r"*"]
+ignore_patterns = None
+ignore_directories = False
+case_sensitivity = True
+event_handler = PatternMatchingEventHandler(
+    patterns, ignore_patterns, ignore_directories, case_sensitivity
+)
+observer = Observer()
+observer.schedule(event_handler=event_handler, path=path, recursive=True)
+
+
+def on_created(event):
+    if not re.match(r".*\/node_modules.*", event.src_path):
+        changeList.append(f"{event.src_path} has been created!")
+
+
+event_handler.on_created = on_created
+
+
+def ToggleLogger(start):
+    if start:
+        observer.start()
+    else:
+        if observer.is_alive():
+            observer.stop()
+            observer.join
+            for i in changeList:
+                print(i)
 
 
 # REGION generic functions
@@ -8,6 +46,7 @@ def find(name, path):
     for root, dirs, files in os.walk(path):
         if name in files:
             return os.path.join(root, name)
+
 
 def prependLine(line, initialFile):
     # this function is responsible for adding strings to the top of a file
@@ -18,6 +57,7 @@ def prependLine(line, initialFile):
             writeObj.write(line)
     os.remove(initialFile)
     os.rename(TEMPNAME, initialFile)
+
 
 def globerizeList(fileList):
     # this function is will create a glob list of the provided file list
@@ -30,32 +70,32 @@ def globerizeList(fileList):
             if dirName == "":
                 globList.append(i)
             else:
-             globList.append("./" + dirName + "/**/*.{html, js, jsx, tsx, htm}")
+                globList.append("./" + dirName + "/**/*.{html, js, jsx, tsx, htm}")
     return ListToString(globList), globList
 
 
 def compareDir(stringA, stringB):
-  lengthA = len(stringA)
-  lengthB = len(stringB)
-  result = ""
-  obselete = None
-  cap = 0  # this is where the for loop should stop
-  # at the smallest string
-  if lengthA < lengthB:
-    cap = lengthA
-  else:
-    cap = lengthB
-
-  for i in range(cap):
-    if stringA[i] == stringB[i]:
-      result += stringA[i]
+    lengthA = len(stringA)
+    lengthB = len(stringB)
+    result = ""
+    obselete = None
+    cap = 0  # this is where the for loop should stop
+    # at the smallest string
+    if lengthA < lengthB:
+        cap = lengthA
     else:
-      break
+        cap = lengthB
 
-  if result.startswith("./") and result.endswith("/") and len(result) > 2:
-    obselete = stringB if cap == lengthA else stringA
+    for i in range(cap):
+        if stringA[i] == stringB[i]:
+            result += stringA[i]
+        else:
+            break
 
-  return obselete
+    if result.startswith("./") and result.endswith("/") and len(result) > 2:
+        obselete = stringB if cap == lengthA else stringA
+
+    return obselete
 
 
 def minifyGlobDir(globedList):
@@ -68,16 +108,16 @@ def minifyGlobDir(globedList):
                     obseleteGlobList.append(obseleteGlob)
     for i in obseleteGlobList:
         globedList.remove(i)
-    
+
     return ListToString(globedList)
 
 
-def ListToString(list, seperator=", \n", string = True):
+def ListToString(list, seperator=", \n", string=True):
     result = None
     if string:
-        result = [f"\"{i}\"" for i in list]
+        result = [f'"{i}"' for i in list]
     result = seperator.join(result)
     return result
 
-# ENDREGION
 
+# ENDREGION
